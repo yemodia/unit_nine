dc5d43a7f7f7bf0379c2e09a28a5adf7ce631365
Yerim <yerim@iMac6.local>
2018-11-29 10:17:01 -0500

    I added the compare function and was able to make the program return the winning card

--- assignment_nine.py
+++ assignment_nine.py
@@ -6,20 +6,28 @@ def deal_cards(d):
     player_one_cards = []
     player_two_cards = []
 
-    for x in range(4):
-       player_one_cards.append(d.draw_a_card())
-       player_two_cards.append(d.draw_a_card())
+    for x in range(5):
+        player_one_cards.append(d.draw_a_card())
+        player_two_cards.append(d.draw_a_card())
+
+    return player_one_cards, player_two_cards
 
 
-def compare_cards():
-    pass
+def compare_cards(card_one, card_two):
+    compare = card_one.compared_to(card_two)
+    return compare
 
-    
+
 def main():
     deck_of_cards = deck.Deck()
     deck_of_cards.shuffle()
-    deal_cards(deck_of_cards)
-
-
+    card_list_one, card_list_two = deal_cards(deck_of_cards)
+    player_one_points = 0
+    player_two_points = 0
+    for x in range (5):
+        print("Player one has", card_list_one[x])
+        print("Player two has", card_list_two[x])
+        print("The winning card is:", compare_cards(card_list_one[x], card_list_two[x]))
+        print(" ")
 
 main()
